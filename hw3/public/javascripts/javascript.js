9dc76d449b1c30b71944c2b31ac85d72353e95e4
LKenny26 <kenny26@up.edu>
2024-02-14 23:30:53 -0800

    all months have diff values

--- hw3/public/javascripts/javascript.js
+++ hw3/public/javascripts/javascript.js
@@ -2,12 +2,13 @@
 
 $(document).ready(function() {
     $("#orderButton").click(function(){
-        var note = $('#notes').val().toLowerCase();
+        var note = $('#notes').val().toLowerCase(); //make notes to lower to scan for vegan
 
         if(note.includes('vegan')) {
+            //dont let a vegan order one
             alert("Warning this has dairy!");
         } else {
-        
+            //hide the form elements when order button clicked
             $(this).hide();
             $(quan).hide();
             $(notes).hide();
@@ -16,6 +17,8 @@ $(document).ready(function() {
             $(ty).show();
         }
     });
+
+    //changes the month
     $("#1").click(function(){
         $("#drop").text("Jan");
         post_order("Jan");
@@ -23,55 +26,72 @@ $(document).ready(function() {
     
     $("#2").click(function(){
         $("#drop").text("Feb");
+        post_order("Feb");
     });
     $("#3").click(function(){
         $("#drop").text("Mar");
+        post_order("Mar");
     });
     $("#4").click(function(){
         $("#drop").text("Apr");
+        post_order("Apr");
     });
     $("#5").click(function(){
         $("#drop").text("May");
+        post_order("May");
     });
     $("#6").click(function(){
         $("#drop").text("Jun");
+        post_order("Jun");
     });
     $("#7").click(function(){
         $("#drop").text("Jul");
+        post_order("Jul");
     });
     $("#8").click(function(){
         $("#drop").text("Aug");
+        post_order("Aug");
     });
     $("#9").click(function(){
         $("#drop").text("Sep");
+        post_order("Sep");
     });
     $("#10").click(function(){
         $("#drop").text("Oct");
+        post_order("Oct");
     });
     $("#11").click(function(){
         $("#drop").text("Nov");
+        post_order("Nov");
     });
     $("#12").click(function(){
         $("#drop").text("Dec");
+        post_order("Dec");
     });
 });
 
+//function to make post go through based on which month called it
 function post_order(month) {
-    $.post('/orders', { month: month })
-    .done(function(data){
-        $("#orderList").empty();
-        data.data.forEach(function(order) {
+    $.post('/orders', { month: month }, function(data) {
+        //post has been called and data recieved
+        $("#orderList").empty(); //clear out the old info
+        data.forEach(function(order) {
+            //need to call this on each flavor
             if (order.topping == "Cherry") {
+                //cherry CSS added
                 $("#orderList").append("<li id=Cherry>" + order.quantity + " " + order.topping + "</li>");
             }
             else if (order.topping == "Chocolate") {
+                //Chocolate CSS added
                 $("#orderList").append("<li id=Choco>" + order.quantity + " " + order.topping + "</li>");
             }
             else {
+                //plain CSS added
                 $("#orderList").append("<li id=Plain>" + order.quantity + " " + order.topping + "</li>");
             }
         });
     }).fail(function(){
+        //if it doesn't work report error
         alert("Error!!!");
     });
 }
--- hw3/routes/orders.js
+++ hw3/routes/orders.js
@@ -1,21 +1,33 @@
 // @author LeeAhnna Kenny 
 const express = require('express');
 const router = express.Router();
-var one ={ topping:"Cherry", quantity:"12" };
-var two= { topping:"Chocolate", quantity:"2" };
-var three= { topping:"Plain", quantity:"7" };
-
-//add the data 
-var Jan = [];
-Jan.push(one);
-Jan.push(two);
-Jan.push(three);
 
 
+const ordersData = {
+    //array of all of the months orders, quantity and topping are here 
+    Jan: [{ topping:"Cherry", quantity:"11" },{ topping:"Chocolate", quantity:"17" },{ topping:"Plain", quantity:"31" }],
+    Feb: [{ topping:"Cherry", quantity:"8" },{ topping:"Chocolate", quantity:"6" },{ topping:"Plain", quantity:"3" }],
+    Mar: [{ topping:"Cherry", quantity:"2" },{ topping:"Chocolate", quantity:"4" },{ topping:"Plain", quantity:"8" }],
+    Apr: [{ topping:"Cherry", quantity:"5" },{ topping:"Chocolate", quantity:"0" },{ topping:"Plain", quantity:"9" }],
+    May: [{ topping:"Cherry", quantity:"0" },{ topping:"Chocolate", quantity:"3" },{ topping:"Plain", quantity:"11" }],
+    Jun: [{ topping:"Cherry", quantity:"3" },{ topping:"Chocolate", quantity:"10" },{ topping:"Plain", quantity:"12" }],
+    Jul: [{ topping:"Cherry", quantity:"1" },{ topping:"Chocolate", quantity:"3" },{ topping:"Plain", quantity:"6" }],
+    Aug: [{ topping:"Cherry", quantity:"8" },{ topping:"Chocolate", quantity:"16" },{ topping:"Plain", quantity:"19" }],
+    Sep: [{ topping:"Cherry", quantity:"6" },{ topping:"Chocolate", quantity:"3" },{ topping:"Plain", quantity:"9" }],
+    Oct: [{ topping:"Cherry", quantity:"4" },{ topping:"Chocolate", quantity:"9" },{ topping:"Plain", quantity:"4" }],
+    Nov: [{ topping:"Cherry", quantity:"7" },{ topping:"Chocolate", quantity:"7" },{ topping:"Plain", quantity:"5" }],
+    Dec: [{ topping:"Cherry", quantity:"9" },{ topping:"Chocolate", quantity:"8" },{ topping:"Plain", quantity:"1" }]
+}
 
-router.post('/', function(req, res, next) {
-    
-    res.json({data:Jan});
+
+
+router.post('/', (req, res) => {
+    //make a month variable that takes on what is passed through
+    const { month } = req.body;
+    //take the month and find it in the orders array
+    const monthData = ordersData[month] || [];
+    //pass the month's data as a JSON
+    res.json(monthData);
 });
 
 module.exports = router;
